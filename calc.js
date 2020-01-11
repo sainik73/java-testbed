ac5eef731e1dc932b5a1fe83dae2715649ac46fa
Kiran <54754326+sainik73@users.noreply.github.com>
2020-01-11 00:58:34 -0800

    Fix bug - calc function was doubling the product of 2 numbers

--- calc.js
+++ calc.js
@@ -1,3 +1,5 @@
+#This function is use to calculate the product of 2 numbers
+# 01/11: Fix bug - calc function was doubling the product of 2 numbers
 function calc(a, b) {
-  return 2 * a * b;             // Function returns the product of a and b
+  return a * b;             // Function returns the product of a and b
 }
