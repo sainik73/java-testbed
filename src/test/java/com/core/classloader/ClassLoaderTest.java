39971e6036bb875078fbe36382e39b5327b66a44
Kiran <sainik73@users.noreply.github.com>
2020-02-14 12:34:42 -0800

    no message

--- src/test/java/com/core/classloader/ClassLoaderTest.java
+++ src/test/java/com/core/classloader/ClassLoaderTest.java
@@ -4,15 +4,9 @@ public class ClassLoaderTest {
 
 	public static void main(String[] args) {
 
-		/*
-		* Bootstrap Classloader for java.lang classes is written
-		* in native language. So getClassLoader() will return Null
-		*/
-		System.out.println("class loader for HashMap: "
+		System.out.println("class loader for bootstrap classes/HashMap: "
 				+ java.util.HashMap.class.getClassLoader());
-		/* Extensions classloader */
 		System.out.println("class loader for ZipInfo: " + com.sun.nio.zipfs.ZipInfo.class.getClassLoader());
-		/* Application class loader */
 		System.out.println("class loader for this class: "
 				+ ClassLoaderTest.class.getClassLoader());
 
