8064e12c165c7b36899d95b3976f37e047a0c4c1
Kiran Saini <sainik73@users.noreply.github.com>
2020-09-13 13:32:05 -0700

    Added example for IntFunction

--- src/main/java/com/java/examples/function/ComposeAndThenExample.java
+++ src/main/java/com/java/examples/function/ComposeAndThenExample.java
@@ -1,24 +1,41 @@
 package com.java.examples.function;
 
 import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+//@see java.util.function.Function Interface
 public class ComposeAndThenExample {
     private static Logger logger = Logger.getLogger(ComposeAndThenExample.class.getName());
+
     public static void main(String[] args) {
         Function<Integer, Integer> add = v -> v + 3;
         Function<Integer, Integer> multiply = v -> v * 2;
+
+        Function<Integer, String> addAndReturnStringOutput = v -> ("Output is " +v + 3);
+
+        String SPACER = "------------------------------------";
 
         // AndThen applies function first, then applies parameters
         Function<Integer, Integer> multiplyThenAdd = multiply.andThen(add);
         Integer result = multiplyThenAdd.apply(3);
         logger.log(Level.INFO,"Result multiplyThenAdd: {0}", result);
 
+        logger.log(Level.INFO,SPACER);
         // Compose applies parameters first and then calls function
         Function<Integer,Integer> multiplyComposeAdd = multiply.compose(add);
         result = multiplyComposeAdd.apply(3);
         logger.log(Level.INFO,"Result multiplyComposeAdd : {0}", result);
+
+        logger.log(Level.INFO,SPACER);
+        logger.log(Level.INFO, "Result Function<Integer, String> : {0}", addAndReturnStringOutput.apply(3));
+
+        logger.log(Level.INFO,SPACER);
+        IntFunction<Double> ob = a -> a / 2.0;
+        // Using apply() method
+        logger.log(Level.INFO,"Result IntFunction : {0}",ob.apply(3));
+
     }
 
 }
